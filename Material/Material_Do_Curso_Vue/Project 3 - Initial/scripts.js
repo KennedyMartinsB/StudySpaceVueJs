88a9bb2c171b1a34ec4dfd03fc4ee930e1552197
KennedyMartinsB <kenedy.martins185@gmail.com>
2023-11-08 17:02:59 -0300

    Feature: lifecycle hooks

--- Material/Material_Do_Curso_Vue/Project 3 - Initial/scripts.js
+++ Material/Material_Do_Curso_Vue/Project 3 - Initial/scripts.js
@@ -1,25 +1,25 @@
 
-var todos = [
-    {
-        text: 'Aprender HTML, CSS e Javascript',
-        done: true
-        // A variável done representa o estado da tarefa 
-    },
-    {
-        text: 'Aprender o básico de Vue JS',
-        done: false
-    },
-    {
-        text: 'Completar o desafio de Vue JS com excelência',
-        done: false
-    }
-];
+// var todos = [
+//     {
+//         text: 'Aprender HTML, CSS e Javascript',
+//         done: true
+//         // A variável done representa o estado da tarefa 
+//     },
+//     {
+//         text: 'Aprender o básico de Vue JS',
+//         done: false
+//     },
+//     {
+//         text: 'Completar o desafio de Vue JS com excelência',
+//         done: false
+//     }
+// ];
 
 const todosApp = {
     data() {
         return {
             // Puxando os valores do array de dados criado acima
-            todos: window.todos,
+            todos: [],
             newTodo: {
                 done: false
             }
@@ -27,11 +27,27 @@ const todosApp = {
     },
     methods: {
         addToDo: function(){
-            this.todos.push(this.newTodo);
-            this.newTodo = {
-                done: false
-            };
+            if(this.newTodo.text){                
+                this.todos.push(this.newTodo);
+                this.newTodo = {
+                    done: false
+                };
+                localStorage.setItem("Tasks", JSON.stringify(this.todos))
+            }
+            else{
+                alert("A tarefa não pode estar vazia!!!")
+            }
         }
+    },
+    // Lifecycle Hooks
+    beforeCreate() {
+        console.log("Before create", this.todos)
+    },
+    created() {
+        console.log("Created", this.todos)
+    },
+    beforeUpdate() {
+        console.log("Before update", this.todos)
     },
 };
 
