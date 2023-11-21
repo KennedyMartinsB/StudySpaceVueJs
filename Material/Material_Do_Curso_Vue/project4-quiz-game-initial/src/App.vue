d75e498d3e8fa01790e1aa6c9bf6febaa4ab432b
KennedyMartinsB <kenedy.martins185@gmail.com>
2023-11-20 21:16:08 -0300

    Feature: conclusão do projeto 4 quiz

--- Material/Material_Do_Curso_Vue/project4-quiz-game-initial/src/App.vue
+++ Material/Material_Do_Curso_Vue/project4-quiz-game-initial/src/App.vue
@@ -1,35 +1,115 @@
 <template>
   <div>
-    <h1>
-      Microphones can be used not only to pick up sound, but also to project sound similar to a speaker.
-    </h1>
 
-    <input type="radio" name="option" id="true">
-    <label>True</label>
+    <ScoreBoard :winCount="this.winCount" :loseCount="this.loseCount"/>
 
-    <br>
+    <template v-if="this.question">
+      <h1 v-html="this.question">
+      </h1>
 
-    <input type="radio" name="option" id="true">
-    <label>False</label>
+      <template v-for="(answer, index) in this.answers" v-bind:key="index">
+        <input 
+          :disabled="this.answerSubmitted"
+          type="radio" 
+          name="options" 
+          :value="answer" 
+          v-model="this.chosenAnswer"
+          >        
+        <label v-html="answer"></label><br>
 
-    <br>
+      </template>
+      <button class="send" v-if="!this.answerSubmitted" type="button" @click="submitAnswer()">Confirmar</button>
+    </template>
 
-    <button class="send" type="button">
-      Send
-    </button>
+      <section v-if="this.answerSubmitted" class="result">
+        <template v-if="this.chosenAnswer == this.correctAnswer">
+          <h4>&#9989; Parabéns, a resposta "{{this.correctAnswer}}" está correta.</h4>
+        </template>
+        <template v-else>
+          <h4>&#10060;  Que pena, a resposta está errada. A resposta correta é "{{this.correctAnswer}}".</h4>
+        </template>
+        <button class="send" @click="this.getNewQuestion()"  type="button">Próxima pergunta</button>
+      </section>
   </div>
 </template>
 
 <script>
-
-// URL da API: https://opentdb.com/api.php?amount=1&category=12&difficulty=easy
+// URL da API multipla escolha: https://opentdb.com/api.php?amount=1&category=12&difficulty=easy
+// URL API true or false: https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=boolean
+import ScoreBoard from '@/components/ScoreBoard.vue'
 
 export default {
+
   name: 'App',
   components: {
-    
-  }
+    ScoreBoard
+  },
+    data() {
+      return {
+        question: undefined,
+        incorrectAnswers: undefined,
+        correctAnswer: undefined,
+        chosenAnswer: undefined,
+        answerSubmitted: false,
+        winCount: 0,
+        loseCount: 0
+      }
+    },
+    computed: {
+      answers() {
+        // As respostas incorretas fazem parte de um array
+        var answers = JSON.parse(JSON.stringify(this.incorrectAnswers));
+        // Metodo splice adiciona um elemento dentro do array e quantos elementos desejamos remover
+        // O primeiro valor do splice corresponde a qual posição iremos adicionar o elemento
+        // O segundo valor corresponde a quantidade de elementos que queremos remover nesse caso é 0
+        // Nesta linha estamos adicionando a resposta correta em uma posição aleatória dentro do array
+        // Math.random()*4 Significa que os valores sorteados vão de 0 até o tamanho do array
+        answers.splice( Math.round(Math.random() * answers.length), 0, this.correctAnswer);
+        return answers
+      }
+    },
+    methods: {
+      // Metodo de envio de resposta, valida se ela está correta ou não
+      submitAnswer(){
+        if(!this.chosenAnswer){
+          alert("Selecione uma alternativa")
+        } else{
+          // Troca de estado do input
+          this.answerSubmitted = true
+          if(this.chosenAnswer == this.correctAnswer){
+            console.log("Resposta correta "+ this.chosenAnswer)
+            this.winCount++;
+            console.log(this.winCount)
+          } else{
+            console.log("Você errou")
+            this.loseCount++;
+          }
+        }
+      },
+      // Metodo de get na API
+      getNewQuestion(){
+
+        // Reiniciando as variáveis para ser possivel selecionar outro radio na proxima pergunta
+        // Resetar a mensagem de acerto ou erro a pergunta
+        this.chosen_answer = undefined;
+        this.answerSubmitted = false;
+        this.question = undefined;
+
+        const api = 'https://opentdb.com/api.php?amount=1&category=12&difficulty=easy'
+        this.axios.get(api).then((response)=> {
+        this.question = response.data.results[0].question;
+        this.incorrectAnswers = response.data.results[0].incorrect_answers;
+        this.correctAnswer = response.data.results[0].correct_answer;
+        // console.log(response.data.results[0])
+    })
+      }
+    },
+    created() {
+      this.getNewQuestion();
+    }
 }
+
+
 </script>
 
 <style lang="scss">
