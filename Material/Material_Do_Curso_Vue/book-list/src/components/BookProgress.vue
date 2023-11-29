cb87ac1c9102e07e82c231cbc85b657bbff1d643
KennedyMartinsB <kenedy.martins185@gmail.com>
2023-11-29 08:44:38 -0300

    Feature: conclusão do curso

---
+++ Material/Material_Do_Curso_Vue/book-list/src/components/BookProgress.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { computed } from "vue"
+
+
+    const props = defineProps(['books'])
+
+    const booksRead = computed(() => {
+        return props.books.filter(book => book.isRead);
+    })
+
+    const booksReadMessage = computed(() => {
+        return booksRead.value >= props.book.length ? 'Parabens você leu todos os livros' : `${booksRead.value} de ${books.length} livros lidos`;
+    })
+
+</script>
+
+<template>
+    <div class="books-read">
+        <label for="progress">Seu Progresso</label>
+        <br>
+        <progress :value="booksRead" :max="books.length"></progress>
+        <p>{{booksReadMessage}}</p>
+    </div>
+</template>
